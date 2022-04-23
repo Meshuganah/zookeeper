ad7b9abcc8ea13c116d8085094b242372b628c45
Josh Klein <kleinjjosh74@gmail.com>
2022-04-23 15:12:00 -0400

    added ability to post new animals to server, as well as validation for doing so

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const fs = require('fs');
+const path = require('path');
+
+//Parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+//Parse incoming JSON data
+app.use(express.json());
 
 const { animals } = require('./data/animals');
 
@@ -33,7 +40,7 @@ function filterByQuery(query, animalsArray) {
     };
 
     if (query.diet) {
-        filteredResults = filteredResults.filter(animal => animals.diet === query.diet);
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
     }
     if (query.species) {
         filteredResults = filteredResults.filter(animal => animal.species === query.species);
@@ -60,6 +67,36 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
+//Pushes the newly added animal to the local animalsArray, and then adds the new animal to the JSON file on the server as well
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray}, null, 2)
+    );
+
+    return animal;
+};
+
+//Validation function
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== "string") {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== "string") {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== "string") {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+};
+
 //Used to filter 1 specific animal based on ID
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
@@ -71,10 +108,20 @@ app.get('/api/animals/:id', (req, res) => {
   });
 
 //Handles users POSTing new information to the database
-app.post('./api/animals', (req, res) =>{
+app.post('/api/animals', (req, res) => {
     // req.body is where our incoming content will be
-    console.log(req.body);
-    res.json(req.body);
+    //Set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    //If any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send(`The animal is not properly formatted.`);
+    } else {
+        //Add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+
+        res.json(animal);
+    };
 });
 
 app.listen(PORT, () => {
